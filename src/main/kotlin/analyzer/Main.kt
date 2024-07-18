081d216a5b959b05b656d8361558e5b2088bb7de
Mike Zak <feanorr@gmail.com>
2024-07-18 23:40:52 +0300

    loadPiece now works

--- src/main/kotlin/analyzer/Main.kt
+++ src/main/kotlin/analyzer/Main.kt
@@ -1,5 +1,37 @@
 package analyzer
 
-fun main() {
-    println("Henlo")
+import java.io.File
+
+fun main(args: Array<String>) {
+    val filename = getFilename(args)
+    if (filename == "") {
+        return
+    }
+
+    val piece = loadPiece(filename)
+
+    println("Instruments: ${piece.instruments.joinToString(", ") { it.name }}")
+    println("Bars:")
+    piece.bars.forEach { bar ->
+        println("Bar ${bar.number}:")
+        bar.registerMap.forEach { (instrument, register) ->
+            println("  ${instrument.name}: ${register.numbersToProportions}")
+        }
+    }
+}
+
+private fun getFilename(args: Array<String>): String {
+    if (args.isEmpty()) {
+        println("Please provide a filename as the first argument.")
+        return ""
+    }
+
+    val filename = args[0]
+    val file = File(filename)
+
+    if (!file.exists() || file.isDirectory) {
+        println("The provided filename is not valid or it's a directory.")
+        return ""
+    }
+    return filename
 }
--- src/main/kotlin/analyzer/loadPiece.kt
+++ src/main/kotlin/analyzer/loadPiece.kt
@@ -3,26 +3,29 @@ package analyzer
 import java.io.File
 
 fun loadPiece(path: String): Piece {
-    val bars = mutableListOf<Bar>()
-
     File(path).useLines { lines ->
-        val header = lines.first()
+        val iterator = lines.iterator()
+        val header = iterator.next()
 
         val instrumentNames = header.split(",").drop(1)
         val instruments = instrumentNames.map { instrumentName -> Instrument(instrumentName) }
 
-        lines.map { line ->
+        val bars = iterator.asSequence().map { line ->
             val values = line.split(",")
             return@map Bar(
                 number = values[0].toInt(),
                 registerMap = instruments.zip(values.drop(1).map { parseRegister(it) }).toMap()
             )
         }
-        return Piece(instruments, bars)
+        return Piece(instruments, bars.toList())
     }
 }
 
 fun parseRegister(registerString: String): Register {
+    if (registerString == "-") {
+        return Register(sortedMapOf())
+    }
+
     if (registerString.contains("<->")) {
         val split = registerString.split("<->")
         val (top, bottom, _) = split.map { it.toInt() }
@@ -31,19 +34,19 @@ fun parseRegister(registerString: String): Register {
             (bottom..top).associateWith { 1.0 / total }
                 .toSortedMap { a, b -> a.compareTo(b) }
         return Register(numbersToProportions)
-    } else {
-        val split = registerString.split("->")
-        val numbersToProportions = mutableMapOf<Int, Double>()
-        val proportionUnit = 1.0 / split.size
-        for (registerString in split) {
-            val registerNumber = registerString.toInt()
-            if (numbersToProportions.contains(registerNumber)) {
-                numbersToProportions[registerNumber] =
-                    numbersToProportions[registerNumber]!! + proportionUnit
-            } else {
-                numbersToProportions[registerNumber] = proportionUnit
-            }
-        }
-        return Register(numbersToProportions.toSortedMap { a, b -> a.compareTo(b) })
     }
+
+    val split = registerString.split("->")
+    val numbersToProportions = mutableMapOf<Int, Double>()
+    val proportionUnit = 1.0 / split.size
+    for (registerString in split) {
+        val registerNumber = registerString.toInt()
+        if (numbersToProportions.contains(registerNumber)) {
+            numbersToProportions[registerNumber] =
+                numbersToProportions[registerNumber]!! + proportionUnit
+        } else {
+            numbersToProportions[registerNumber] = proportionUnit
+        }
+    }
+    return Register(numbersToProportions.toSortedMap { a, b -> a.compareTo(b) })
 }
